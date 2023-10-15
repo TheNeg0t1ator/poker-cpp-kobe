a75e52d6d3aeef29f6978a0657e18b18d6520758
Kobe Dieryck <kobe.dieryck@icloud.com>
2023-10-15 15:50:24 +0200

    added function moneyCopyCat

--- main.cpp
+++ main.cpp
@@ -5,6 +5,10 @@
 #include "tournament.h"
 #include "bart.h"
 #include "kobed.h"
+#include "seppe.h"
+#include "dries.h"
+#include "ceyhan.h"
+#include "pxl_2023_samy.h"
 #include "king_of_aces.h"
 
 using namespace std;
@@ -17,8 +21,12 @@ int main( int argumentCount, char * argumentVector[] )
     for( unsigned char i = 0 ; i < 4 ; i++ )
     {
         tour.registerPlayer( new PXL2022::KingOfAces( i ), PXL_2022 );
+        tour.registerPlayer( new PXL2023::Dries( i ), PXL_2023_DRIES);
         tour.registerPlayer( new PXL2022::Bart( i ), PXL_2021 );
         tour.registerPlayer( new PXL2023::KobeD( i ), PXL_2023_KOBE);
+        tour.registerPlayer( new PXL2023::seppe( i ), PXL_2023_SEPPE);
+        tour.registerPlayer( new PXL2023::PXL_2023_SAMY( i ), PXL_2023_SAMY);
+        tour.registerPlayer( new PXL2023::Ceyhan( i ), PXL_2023_CEY);
 	}
 
 	if( argumentCount > 1 && ( strcmp( argumentVector[argumentCount-1], "stats" ) == 0 ) )
