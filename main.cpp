cfa8c6f1fe89d1640e14870f8951ef32ac4cbccb
Kobe Dieryck <kobe.dieryck@icloud.com>
2023-10-15 15:32:47 +0200

    added function moneyCopyCat

--- main.cpp
+++ main.cpp
@@ -20,13 +20,9 @@ int main( int argumentCount, char * argumentVector[] )
 
     for( unsigned char i = 0 ; i < 4 ; i++ )
     {
-        //tour.registerPlayer( new PXL2022::KingOfAces( i ), PXL_2022 );
-        //tour.registerPlayer( new PXL2023::Dries( i ), PXL_2023_DRIES);
-        //tour.registerPlayer( new PXL2022::Bart( i ), PXL_2021 );
+        tour.registerPlayer( new PXL2022::KingOfAces( i ), PXL_2022 );
+        tour.registerPlayer( new PXL2022::Bart( i ), PXL_2021 );
         tour.registerPlayer( new PXL2023::KobeD( i ), PXL_2023_KOBE);
-        tour.registerPlayer( new PXL2023::seppe( i ), PXL_2023_SEPPE);
-        tour.registerPlayer( new PXL2023::PXL_2023_SAMY( i ), PXL_2023_SAMY);
-        tour.registerPlayer( new PXL2023::Ceyhan( i ), PXL_2023_CEY);
 	}
 
 	if( argumentCount > 1 && ( strcmp( argumentVector[argumentCount-1], "stats" ) == 0 ) )
